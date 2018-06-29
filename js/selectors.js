bb219fc2cfa76b57ff17d12c2816e982354ac25e
Serena Thomas <serenathomas96@gmail.com>
2018-06-29 03:40:53 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,20 @@
 // declare your functions here...
+function paragraphSelector() {
+  $('p')
+}
+
+function lastImageSelector() {
+  $('div:last')
+}
+
+function ninjaBabySelector(){
+  $('#baby-ninja')
+}
+
+function divSelector() {
+  $('.pics')
+}
+
+function firstListItem() {
+  $('div img:first-child')
+}
